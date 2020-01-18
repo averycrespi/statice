0bc96bdabddf1c190a4635283d9aef0fad99946d
Avery Crespi <averycrespi@gmail.com>
2020-01-18 15:25:56 -0800

    Add inbound and outbound tables

--- app/core/routes.py
+++ app/core/routes.py
@@ -6,8 +6,19 @@ from app.core import bp
 @bp.route("/")
 @bp.route("/dashboard")
 def dashboard():
+    from collections import namedtuple
+
+    Check = namedtuple("Check", ("name", "ok", "url", "last_updated"))
     checks = {
-        "foo": True,
-        "bar": False,
+        Check(
+            name="Plex",
+            ok=True,
+            url="plex.averycrespi.com",
+            last_updated="3 seconds ago",
+        ),
+        Check(
+            name="FTP", ok=False, url="ftp.averycrespi.com", last_updated="10 hours ago"
+        ),
     }
-    return render_template("dashboard.html", checks=checks)
+
+    return render_template("dashboard.html", outbound_checks=checks)
