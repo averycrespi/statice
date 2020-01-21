cbda6207e4edab577f4e18ff546c58a2c0860955
Avery Crespi <averycrespi@gmail.com>
2020-01-20 21:00:10 -0800

    Update dashboard and settings pages

---
+++ app/settings/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class CreateCheckForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    url = StringField("URL", validators=[DataRequired()])
+    submit = SubmitField("Create")
+
+    def validate_name(self, name):
+        if Check.query.filter_by(name=name.data).first() is not None:
+            raise ValidationError("Please use a unique check name.")
--- app/settings/routes.py
+++ app/settings/routes.py
@@ -1,8 +1,18 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 
+from app import db
+from app.models import Check
 from app.settings import bp
+from app.settings.forms import CreateCheckForm
 
 
-@bp.route("/settings")
+@bp.route("/settings", methods=["GET", "POST"])
 def settings():
-    return render_template("settings.html")
+    form = CreateCheckForm()
+    if form.validate_on_submit():
+        check = Check(name=form.name, url=form.url)
+        db.session.add(check)
+        db.session.commit()
+        flash(f"Check '{check.name}' has been created.")
+        return redirect(url_for("settings.settings"))
+    return render_template("settings.html", checks=Check.query.all(), form=form)
