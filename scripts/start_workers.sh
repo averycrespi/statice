332756ec4687dc79ba5d0c002cfa4bdde86e4723
Avery Crespi <averycrespi@gmail.com>
2020-02-17 12:09:25 -0800

    Change worker names

--- scripts/start_workers.sh
+++ scripts/start_workers.sh
@@ -4,7 +4,8 @@ N="${STATICE_NUM_WORKERS:-1}"
 
 for i in `seq 1 $N`
 do
-    flask rq worker --name "worker_$(date +%s)" &
+    # Use current unix time as random name.
+    flask rq worker --name "worker_$(date +%s%N)" &
 done
 
 wait
