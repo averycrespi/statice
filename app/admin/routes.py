3fbd5abdc589ea1f1f876fbc617c75df78ec23c7
Avery Crespi <averycrespi@gmail.com>
2020-02-17 16:20:12 -0800

    Fix edit form crash

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -34,8 +34,6 @@ def edit_check(id):
         abort(404)
     form = EditCheckForm(obj=check)
     if form.validate_on_submit():
-        if form.cancel.data:
-            return redirect(url_for("admin.manage_checks"))
         check.name = form.name.data
         check.url = form.url.data
         db.session.add(check)
