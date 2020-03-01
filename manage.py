acf02694989e7ee4edb2aff45a9210b52d6b6eac
Avery Crespi <averycrespi@gmail.com>
2020-03-01 15:28:24 -0800

    Fix build errors

--- manage.py
+++ manage.py
@@ -9,8 +9,8 @@ app = create_app(Config)
 @app.cli.command("daemon")
 def daemon():
     """Run the daemon."""
-    daemon = Daemon()
-    daemon.start(
+    daemon = Daemon(
         interval=app.config["STATICE_DAEMON_INTERVAL"],
         timeout=app.config["STATICE_REQUEST_TIMEOUT"],
     )
+    daemon.start()
