0e525a7075806830c62112f1fa03a64ecdf82a42
Avery Crespi <averycrespi@gmail.com>
2020-03-09 19:57:30 -0700

    Change default config values

--- app/config.py
+++ app/config.py
@@ -2,26 +2,29 @@ import os
 
 
 class Config:
-    """Configures the application for development or production."""
+    """Configures the application for Docker."""
 
     # Configure application-specific behaviour.
-    STATICE_DAEMON_INTERVAL = int(os.environ.get("STATICE_DAEMON_INTERVAL") or 30)
-    STATICE_REQUEST_TIMEOUT = int(os.environ.get("STATICE_REQUEST_TIMEOUT") or 5)
+    STATICE_DAEMON_INTERVAL = os.environ.get("STATICE_DAEMON_INTERVAL", 30)
+    STATICE_REQUEST_TIMEOUT = os.environ.get("STATICE_REQUEST_TIMEOUT", 5)
 
-    # If a secret key is not set, generate a random key on startup.
+    # Configure Flask.
     SECRET_KEY = os.environ.get("SECRET_KEY") or os.urandom(24).hex()
 
-    POSTGRES_URL = os.environ.get("POSTGRES_URL") or "localhost:5432"
+    # Configure PostgreSQL.
+    POSTGRES_URL = os.environ.get("POSTGRES_URL") or "statice_db:5432"
     POSTGRES_DB = os.environ.get("POSTGRES_DB") or "postgres"
     POSTGRES_USER = os.environ.get("POSTGRES_USER") or "postgres"
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD") or "postgres"
 
+    # Configure SQLAlchemy.
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://{user}:{pw}@{url}/{db}".format(
         user=POSTGRES_USER, pw=POSTGRES_PASSWORD, url=POSTGRES_URL, db=POSTGRES_DB
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    RQ_REDIS_URL = os.environ.get("REDIS_URL") or "redis://localhost:6379/0"
+    # Configure Redis.
+    RQ_REDIS_URL = os.environ.get("REDIS_URL") or "redis://statice_redis:6379/0"
     RQ_QUEUES = ("default",)
 
 
@@ -31,5 +34,6 @@ class TestingConfig(Config):
     # Use an in-memory SQLite database.
     SQLALCHEMY_DATABASE_URI = "sqlite://"
 
+    # Enable testing.
     TESTING = True
     WTF_CSRF_ENABLED = False
