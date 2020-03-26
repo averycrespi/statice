00dc40671aacd67d6346233773cc2bb9f9eb8ae3
Avery Crespi <averycrespi@gmail.com>
2020-03-26 12:58:48 -0700

    Remove database upgrades

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-flask db upgrade || echo "No upgrade necessary"
-
 if [ "$FLASK_ENV" == "development" ]; then
     echo "Running in development mode"
     flask run --host=0.0.0.0
